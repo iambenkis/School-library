0607fab513c20f258925dba7c2e2e3b6f5d24aa1
benjamin kisenge <benkisenge03@gmail.com>
2022-11-08 19:37:52 +0200

    Add parent_permission variable in super

--- student.rb
+++ student.rb
@@ -1,10 +1,11 @@
 require_relative 'person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
-    @parent_permission = parent_permission
   end
 
   def play_hooky
