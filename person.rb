34b9e3dcccd84c7edf8cb173cd5200f9eb3ab954
benjamin kisenge <benkisenge03@gmail.com>
2022-11-09 18:53:13 +0200

    Fix rubocop offenses

--- person.rb
+++ person.rb
@@ -1,54 +1,61 @@
 class Nameable
+  def initialize(name)
+    @nameable = name
+  end
+
   def correct_name
-    raise NotImplementedError,"#{self.class} has not implemented methods '#{__method__}'"
+    raise NotImplementedError, "#{self.class} has not implemented methods '#{__method__}'"
   end
 end
-
 
 class Decorator < Nameable
   def initialize(nameable)
     @nameable = nameable
+    super
   end
 
   def correct_name
-    return @nameable
+    @nameable
   end
 end
 
 class CapitalizeDecorator < Decorator
   def initialize(nameable)
     @nameable = nameable
+    super
   end
 
   def correct_name
-    return @nameable.correct_name().capitalize()
+    @nameable.correct_name.capitalize
   end
 end
 
 class TrimmerDecorator < Decorator
   def initialize(nameable)
     @nameable = nameable
+    super
   end
+
   def correct_name
-    if @nameable.correct_name().length > 10
-      return @nameable.correct_name().slice(0, 10)
-   else
-    return @nameable.correct_name()
-   end
+    return @nameable.correct_name.slice(0, 10) if @nameable.correct_name.length > 10
+
+    @nameable.correct_name
   end
 end
 
 class Person < Nameable
   attr_accessor :id, :name, :age
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @id = Random.rand(1..1000)
+    super(name)
   end
 
   def correct_name
-    name
+    @name
   end
 
   def can_use_services?
@@ -64,8 +71,8 @@ class Person < Nameable
 end
 
 person = Person.new(16, 'benjamin')
-puts person.correct_name #Display the name
-capitalizedPerson = CapitalizeDecorator.new(person)
-puts capitalizedPerson.correct_name # Capitalize the name
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-puts capitalizedTrimmedPerson.correct_name # Capitalize and Trim the name if its length is greater than 10
+puts person.correct_name # Display the name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name # Capitalize the name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name # Capitalize and Trim the name if its length is greater than 10
