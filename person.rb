b322772a497c4d386cfaadd4573fbe5d1292a8eb
benjamin kisenge <benkisenge03@gmail.com>
2022-11-09 18:37:18 +0200

    Add Decorator to my object

--- person.rb
+++ person.rb
@@ -1,12 +1,54 @@
-class Person
-  attr_accessor :id, :name, :age
+class Nameable
+  def correct_name
+    raise NotImplementedError,"#{self.class} has not implemented methods '#{__method__}'"
+  end
+end
 
+
+class Decorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    return @nameable
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    return @nameable.correct_name().capitalize()
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def initialize(nameable)
+    @nameable = nameable
+  end
+  def correct_name
+    if @nameable.correct_name().length > 10
+      return @nameable.correct_name().slice(0, 10)
+   else
+    return @nameable.correct_name()
+   end
+  end
+end
+
+class Person < Nameable
+  attr_accessor :id, :name, :age
   def initialize(age, name = 'Unknown', parent_permission: true)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @id = Random.rand(1..1000)
-    puts 'Itnit prton'
+  end
+
+  def correct_name
+    name
   end
 
   def can_use_services?
@@ -21,5 +63,9 @@ class Person
   end
 end
 
-p1 = Person.new(16, 'Benjamin')
-puts p1.can_use_services?
+person = Person.new(16, 'benjamin')
+puts person.correct_name #Display the name
+capitalizedPerson = CapitalizeDecorator.new(person)
+puts capitalizedPerson.correct_name # Capitalize the name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+puts capitalizedTrimmedPerson.correct_name # Capitalize and Trim the name if its length is greater than 10
