5c729611bd4acb8e617cbf72660e290bb3346dbf
benjamin kisenge <benkisenge03@gmail.com>
2022-11-09 21:28:18 +0200

    Apply requied changes

--- person.rb
+++ person.rb
@@ -1,43 +1,31 @@
 class Nameable
-  def initialize(name)
-    @nameable = name
-  end
-
   def correct_name
     raise NotImplementedError, "#{self.class} has not implemented methods '#{__method__}'"
   end
 end
 
 class Decorator < Nameable
+  attr_accessor :nameable
+
   def initialize(nameable)
     @nameable = nameable
-    super
+    super()
   end
 
   def correct_name
-    @nameable
+    @nameable.correct_name
   end
 end
 
 class CapitalizeDecorator < Decorator
-  def initialize(nameable)
-    @nameable = nameable
-    super
-  end
-
   def correct_name
     @nameable.correct_name.capitalize
   end
 end
 
 class TrimmerDecorator < Decorator
-  def initialize(nameable)
-    @nameable = nameable
-    super
-  end
-
   def correct_name
-    return @nameable.correct_name.slice(0, 10) if @nameable.correct_name.length > 10
+    return @nameable.correct_name.slice(0, 9) if @nameable.correct_name.length > 10
 
     @nameable.correct_name
   end
@@ -51,7 +39,7 @@ class Person < Nameable
     @age = age
     @parent_permission = parent_permission
     @id = Random.rand(1..1000)
-    super(name)
+    super()
   end
 
   def correct_name
