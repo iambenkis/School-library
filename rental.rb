dbd7120a229ef48c1a174b8baa32e82cf1b0b4fc
benjamin kisenge <benkisenge03@gmail.com>
2022-11-11 00:30:06 +0200

    Create the many-to-many using the intermediate class Rental

---
+++ rental.rb
@@ -0,0 +1,20 @@
+require_relative 'book'
+require_relative 'person'
+
+class Rental
+    attr_accessor :date, :book, :person
+
+    def initialize(date)
+        @date = date
+    end
+
+    def book=(book)
+        @book = book
+        book.rentals.push(self) unless book.rentals.include?(self)
+    end
+
+    def person=(person)
+        @person = person
+        person.rentals.push(self) unless person.rentals.include?(self)
+    end
+end
