c75ee20dcbb7b6e44d75aa1ad92f05f3e13e8dd7
benjamin kisenge <benkisenge03@gmail.com>
2022-11-11 01:18:24 +0200

    Fix rubocop lint

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,5 @@
 class Nameable
-    def correct_name
-        raise NotImplementedError, "#{self.class} has not implemented methods '#{__method__}'"
-    end
+  def correct_name
+    raise NotImplementedError, "#{self.class} has not implemented methods '#{__method__}'"
+  end
 end
