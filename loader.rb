c791a14c60528f4301640a977c9f7463700e0559
benjamin kisenge <benkisenge03@gmail.com>
2022-11-16 03:32:15 +0200

    Update loader file

--- loader.rb
+++ loader.rb
@@ -3,8 +3,8 @@ require './person'
 require './rental'
 require 'json'
 
-class Data_loader
-  def load_books
+
+def load_books
     if File.exist?('./books.json') && !File.read('./books.json').empty?
       JSON.parse(File.read('./books.json')).map do |book|
         Book.new(book['title'], book['author'])
@@ -14,24 +14,28 @@ class Data_loader
     end
   end
 
-  def load_people
-    if File.exist?('./people.json') && !File.read('./people.json').empty?
-      JSON.parse(File.read('./people.json')).map do |person|
-        is_teacher = person.has_key?("specialization")
-        is_teacher ? Teacher.new(person["id"], person["age"], person["specialization"], person["name"]) : Student.new( person["id"], person["age"], person["name"], person["parent_permission"])
-      end
-    else
-      []
+def load_people
+  if File.exist?('./people.json') && !File.read('./people.json').empty?
+    JSON.parse(File.read('./people.json')).map do |person|
+      is_teacher = person.has_key?("specialization")
+      is_teacher ? Teacher.new(person["id"], person["age"], person["specialization"], person["name"]) : Student.new( person["id"], person["age"], person["name"], person["parent_permission"])
     end
-  end
-
-  def load_rentals
-    if File.exist?('./rentals.json') && !File.read('./rentals.json').empty?
-      JSON.parse(File.read('./rentals.json')).map do |rental|
-        Rental.new(rental['id'],rental['date'], rental['book'], rental['person'])
-      end
-    else
-      []
-    end
+  else
+    []
   end
 end
+
+def load_rentals
+  if File.exist?('./rentals.json') && !File.read('./rentals.json').empty?
+    JSON.parse(File.read('./rentals.json')).map do |rental|
+      books = @books.find do |book| book.title == rental['book']['title'] && book.author == rental['book']['author']
+      end
+      person =  @people.find do |person| person.id == rental['person']['id']
+      end
+      Rental.new(rental['date'], books, person)
+    end
+  else
+    []
+  end
+end
+
